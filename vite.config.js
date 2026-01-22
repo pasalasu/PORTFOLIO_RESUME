7b4d278896265628ee16b54e21624c6c210c1dbd
PASALASURESH <124240418+pasalasu@users.noreply.github.com>
2026-01-22 10:24:17 +0530

    Set base path for portfolio resume in Vite config
    Add base configuration for portfolio resume.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.portfolio-resume || "/PORTFOLIO_RESUME"
 })
+
