c5b167caa5d19f03cc0404153d42169a3d5225ac
PASALASURESH <124240418+pasalasu@users.noreply.github.com>
2026-01-22 10:25:12 +0530

    Fix base URL environment variable in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.portfolio-resume || "/PORTFOLIO_RESUME"
+  base: process.env.portfolio || "/PORTFOLIO_RESUME"
 })
+
 
